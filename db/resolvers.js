0cef1d55606d1f5cdcb7d3d795f140219c13f1be
CaroKan <quindioseguro@gmail.com>
2022-11-04 16:41:28 -0500

    Se termina toda la funcionalidad del Modelo Clientes

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,5 +1,6 @@
 const Usuario = require('../models/Usuario')
 const Producto = require('../models/Producto')
+const Cliente = require('../models/Cliente')
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({path:'.env'});
@@ -20,6 +21,7 @@ crearToken= (usuario,secreta)=>{
 const resolvers={
     Query:{
         obtenerUsuario: async (_,{token}) => {
+            console.log(token)
             const usuarioId= await jwt.verify(token,process.env.SECRETA)
 
             return usuarioId
@@ -38,10 +40,46 @@ const resolvers={
             //verificar que el producto exista
             const producto = await Producto.findById(id);
             if(!producto){
-                throw new Error('Producto no encontrado')
+                throw new Error('Producto no encontrado');
             }
             return producto;
+        },
+
+        obtenerClientes: async ()  => {
+            try {
+                const clientes = await Cliente.find({});
+                return clientes;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        obtenerClientesVendedor: async (_,{},ctx)  => {
+            try {
+                const clientes = await Cliente.find({vendedor: ctx.usuario.id.toString()});
+                return clientes;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        obtenerCliente: async (_,{id},ctx)  => {
+            //Verificar si el cliente existe
+            const cliente = await Cliente.findById({id});
+            if(!cliente){
+                throw new Error('Cliente no encontrado')
+            }
+
+            //Verificar quien lo creo
+            if(cliente.vendedor.toString() !==ctx.usuario.id){
+                throw new Error('No tienes las credenciales')
+            }
+
+            return cliente;
+
         }
+
+
     },
     Mutation:{
         nuevoUsuario: async (_,{input}) => {
@@ -120,9 +158,71 @@ const resolvers={
             await Producto.findOneAndDelete({_id:id});
             return "Producto Eliminado";
 
+        },
+        nuevoCliente : async (_, {input},ctx) => {
+            console.log(ctx);
+            const {email} = input
+            
+
+            //Verificar si el cleinte esta creado
+            const cliente = await Cliente.findOne({email});
+            if(cliente){
+                throw new Error('El cliente ya existe en la base de datos')
+            }
+
+            const nuevoCliente = new Cliente(input);
+            //asignar el vendedor
+            nuevoCliente.vendedor=ctx.usuario.id;
+            console.log("Valor del token Cliente ",nuevoCliente)
+            
+            //guardar en la base de datos
+
+          try {
+            
+            const resultado= await nuevoCliente.save();
+            return resultado;
+        
+            } catch (error) {
+                console.log(error);
+            }
+
+        },
+        
+        actualizarCliente: async(_,{id, input},ctx) =>{
+            // verificar si existe el cliente
+            let cliente = await Cliente.findById(id);
+
+            if(!cliente){
+                throw new Error('El cliente no existe en la base de datos');
+            }
+
+            // verificar si el vendedor es quien edita
+            if(cliente.vendedor.toString() !==ctx.usuario.id){
+                throw new Error('No tienes las credenciales')
+            }
+            
+            //actualziar el cliente
+            cliente = await Cliente.findOneAndUpdate({_id: id}, input, {new: true})
+            return cliente;
+        },
+
+        eliminarCliente: async(_,{id},ctx) =>{
+            // verificar si existe el cliente
+            let cliente = await Cliente.findById(id);
+
+            if(!cliente){
+                throw new Error('El cliente no existe en la base de datos');
+            }
+
+            // verificar si el vendedor es quien edita
+            if(cliente.vendedor.toString() !==ctx.usuario.id){
+                throw new Error('No tienes las credenciales')
+            }
+
+            //eliminar el cliente
+            await Cliente.findOneAndDelete({_id:id});
+            return "Cliente Eliminado"
         }
-
-
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -22,6 +22,16 @@ const typeDefs = gql`
         creado: String
      }
 
+    type Cliente{
+        id:ID
+        nombre: String
+        apellido: String
+        empresa: String
+        email: String
+        telefono: String
+        vendedor: ID
+    }
+
     input UsuarioInput{
         nombre: String!
         apellido: String!
@@ -40,6 +50,14 @@ const typeDefs = gql`
         precio: Float!
     }
 
+    input ClienteInput{
+        nombre: String!
+        apellido: String!
+        empresa: String!
+        email: String!
+        telefono: String
+    }
+
     type Query{
         # Obtener usuarios
         obtenerUsuario(token: String!): Usuario
@@ -47,6 +65,11 @@ const typeDefs = gql`
         # Obtener productos
         obtenerProductos: [Producto]
         obtenerProducto(id:ID!): Producto
+
+        #Clientes
+        obtenerClientes: [Cliente]
+        obtenerClientesVendedor: [Cliente]
+        obtenerCliente(id: ID!): Cliente
     }
 
     type Mutation{
@@ -59,6 +82,11 @@ const typeDefs = gql`
         nuevoProducto(input: ProductoInput) : Producto
         actualizarProducto(id:ID!, input : ProductoInput) : Producto
         eliminarProducto(id:ID!) : String
+
+        #Mutations para los Clientes
+        nuevoCliente(input: ClienteInput) : Cliente
+        actualizarCliente(id: ID!, input: ClienteInput) : Cliente
+        eliminarCliente(id: ID!): String
     }
 `;
 
